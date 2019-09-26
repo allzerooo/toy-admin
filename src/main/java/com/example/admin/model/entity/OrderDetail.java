3424e7990867af6ac1da9ab859571f446f024773
chunbong <cdy0266@gmail.com>
2019-09-26 15:41:55 +0900

    Use JPA to automate created and updated

--- src/main/java/com/example/admin/model/entity/OrderDetail.java
+++ src/main/java/com/example/admin/model/entity/OrderDetail.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -16,6 +21,7 @@ import java.time.LocalDateTime;
 @Data
 @Entity
 @ToString(exclude = {"orderGroup", "item"})
+@EntityListeners(AuditingEntityListener.class)
 public class OrderDetail {
 
     @Id
@@ -30,12 +36,16 @@ public class OrderDetail {
 
     private BigDecimal totalPrice;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @CreatedBy
     private String createdBy;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     private String updatedBy;
 
     // OrderDetail N : 1 Item
