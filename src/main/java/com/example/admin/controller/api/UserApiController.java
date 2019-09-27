d0e8f1cf75757244eed9ade938c6ed203f933429
chunbong <cdy0266@gmail.com>
2019-09-27 15:57:40 +0900

    Add generic to the CRUD Interface and Create User Controller

---
+++ src/main/java/com/example/admin/controller/api/UserApiController.java
@@ -0,0 +1,41 @@
+package com.example.admin.controller.api;
+
+import com.example.admin.ifs.CrudInterface;
+import com.example.admin.model.network.Header;
+import com.example.admin.model.network.request.UserApiRequest;
+import com.example.admin.model.network.response.UserApiResponse;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Override
+    @PostMapping("")    // /api/user
+    public Header<UserApiResponse> create(@RequestBody UserApiRequest userApiRequest) {
+
+        return null;
+
+    }
+
+    @Override
+    @GetMapping("{id}") // /api/user/{id}
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("") // /api/user
+    public Header<UserApiResponse> update(@RequestBody UserApiRequest request) {
+
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}")  // /api/user/{id}
+    public Header delete(@PathVariable Long id) {
+
+        return null;
+    }
+
+}
